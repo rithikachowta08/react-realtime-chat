79a8dc322c9ddb75f4ac8037e8f6387ac94729fd
rithika chowta <rithika.chowta08@gmail.com>
2019-06-12 21:35:46 +0530

    Clean storybook

--- stories/ChatInput.stories.js
+++ stories/ChatInput.stories.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ChatInput from '../src/lib/ChatInput/ChatInput';
 
-storiesOf('ChatInput', module).addWithJSX('ChatInput', () => <ChatInput />);
+storiesOf('Chat input field', module).addWithJSX('ChatInput', () => (
+  <ChatInput />
+));
--- stories/ChatMessage.stories.js
+++ stories/ChatMessage.stories.js
@@ -2,27 +2,33 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ChatMessage from '../src/lib/ChatMessage/ChatMessage';
 
-storiesOf('Chat message', module)
-  .addWithJSX('Chat message', () => (
-    <ChatMessage
-      messageText="Hi Lucille, I'm unable to make your proposed time of Fri, Aug 23.  Here are two alternate times I'm available.  Select whichever works for you.hey"
-      fromSelf
-    />
+storiesOf('Chat message bubbles', module)
+  .addWithJSX('Chat message without header', () => (
+    <ChatMessage messageText="Hi there. This is a message." fromSelf />
   ))
   .addWithJSX('Chat message with header', () => (
     <ChatMessage
       userName="john doe"
-      messageText="Hi Lucille, I'm unable to make your proposed time of Fri, Aug 23.  Here are two alternate times I'm available.  Select whichever works for you."
+      messageText="Hi there. This is a message."
       timeStamp="2.08pm"
-      avatarUrl="https://tekagogoresources.blob.core.windows.net/repair-categories/mechanical-room.png"
+      avatarUrl="https://avatars0.githubusercontent.com/u/3451712?s=60&v=4"
       withHeader
     />
   ))
   .addWithJSX('Chat message from self with header', () => (
     <ChatMessage
       fromSelf
-      messageText="Hi Lucille, I'm unable to make your proposed time of Fri, Aug 23.  Here are two alternate times I'm available.  Select whichever works for you."
+      messageText="Hi there. This is a message."
       withHeader
+      timeStamp="2.08pm"
+    />
+  ))
+  .addWithJSX('Chat message with separator', () => (
+    <ChatMessage
+      fromSelf
+      messageText="Hi there. This is a message."
+      withHeader
+      separatorDate="6/12/2019"
       timeStamp="2.08pm"
     />
   ));
