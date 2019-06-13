8fe7fdb1feec032607f79447c7d8fe7cc9dc205e
rithika chowta <rithika.chowta08@gmail.com>
2019-06-13 14:48:49 +0530

    Add switch role for demo purpose

--- src/App.js
+++ src/App.js
@@ -2,7 +2,39 @@ import React from 'react';
 import Chat from './lib';
 import './App.scss';
 
-function App() {
+// Info of participants in the chat
+const user1 = {
+  name: 'Jon',
+  id: '1004',
+  imageUrl:
+    'https://cdn.vox-cdn.com/thumbor/8vF1atBpBCuBB0guzWVeHieVwCA=/99x0:1179x810/920x613/filters:focal(99x0:1179x810):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/46094226/Jon_snow.0.jpg'
+};
+
+const user2 = {
+  name: 'Daenerys',
+  id: '1005',
+  imageUrl:
+    'https://vignette.wikia.nocookie.net/gameofthrones/images/e/ee/QueenDaenerysTargaryenIronThrone.PNG/revision/latest?cb=20190520173137'
+};
+
+const App = () => {
+  // Function to switch current user i.e "you" in the chat and reload the chat in the new role
+  const currentUserId = localStorage.getItem('currentUserId') || user1.id;
+  if (currentUserId !== user1.id || currentUserId !== user2.id) {
+    localStorage.removeItem('currentUserId');
+  }
+  const switchRoles = () => {
+    if (currentUserId === user1.id) {
+      localStorage.setItem('currentUserId', user2.id);
+    } else {
+      localStorage.setItem('currentUserId', user1.id);
+    }
+    window.location.reload();
+  };
+
+  // Get receiver info based on who the current user is
+  const getReceiver = () => (currentUserId === user1.id ? user2 : user1);
+
   return (
     <div className="App">
       <Chat
@@ -15,16 +47,14 @@ function App() {
           messagingSenderId: '249012927295',
           appId: '1:249012927295:web:035ae95584ff77d4'
         }}
-        currentUserId="45878"
-        receiver={{
-          name: 'Rithika',
-          id: '129090',
-          imageUrl:
-            'https://miro.medium.com/fit/c/256/256/2*XRuD351hKGF-w6TdhF0wVw.jpeg'
-        }}
+        currentUserId={currentUserId}
+        receiver={getReceiver()}
       />
+      <button className="btn" onClick={switchRoles}>
+        Switch role
+      </button>
     </div>
   );
-}
+};
 
 export default App;
